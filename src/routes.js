0ea8a4abdab2ed522825b534f69e9bbbe25bf517
Rebecca Souza <rebecca@psafe.com>
2020-07-20 00:39:01 -0300

    structure change

--- src/routes.js
+++ src/routes.js
@@ -13,14 +13,17 @@ function App() {
   return (
     <div className="App">
         <Navbar/>
-        <Switch>
+        <div className='content'>
+           <Switch>
 
-           <Route exact path='/' component={Home}></Route>
-           <Route path='/signin' component={SignIn}></Route>
-           <Route path='/signup' component={SignUp}></Route>
-           <Route path='/profile' component={Profile}></Route>
+            <Route exact path='/' component={Home}></Route>
+            <Route path='/signin' component={SignIn}></Route>
+            <Route path='/signup' component={SignUp}></Route>
+            <Route path='/profile' component={Profile}></Route>
 
-        </Switch>
+          </Switch>
+        </div>
+       
     </div>
   );
 }
